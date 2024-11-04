5b4444dd0a12d9edcf58f9e1fc7a713ca26cdc57
Wellington <welltecnic@hotmail.com>
2024-11-04 11:10:09 -0300

    Adicionando o a api  do server

--- src/routes/Paginas/Exemplo2.jsx
+++ src/routes/Paginas/Exemplo2.jsx
@@ -1,9 +1,62 @@
-
+import { useState } from "react"
 
 export default function Exemplo2() {
+
+  //Hook-useState- manipula o estado da variavel
+  const [cliente, setCliente]=useState({
+    nome:'',
+    email:''
+  })
+
+    //criando a função handleChange( vai pegar o que passar no input)
+    const handleChange=(e)=>{
+      setCliente({...cliente,[e.target.name]:e.target.value})
+    }
+  
+    //criando a função  handleSubmit
+
+const handleSubmit=(e)=>{
+  e.preventDefault()
+  fetch(`https://bd-1-espk-rho.vercel.app/cliente`,{
+    method:"post",
+    headers:{
+      "Content-Type":"application/json",
+    },
+    body:JSON.stringify(cliente),
+  }).then(()=>{
+    alert("Cliente cadastrado com sucesso")
+  })
+
+}
+
+
   return (
-    <div>
-        <h1>Exemplo2</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+    <h1>Cadastro de Cliente</h1>
+    <p>
+      Nome Cliente:
+      <input 
+      type="text"
+      name="nome"
+      placeholder="Digite o Nome do Cliente" 
+      onChange={handleChange}
+      value={cliente.nome}
+    
+       />
+    </p>
+
+    <p>
+      Email Cliente:
+      <input 
+      type="text"
+      name="email"
+      placeholder="Digite o email do cliente" 
+      onChange={handleChange}
+      value={cliente.unidade}
+    
+       />
+    </p>
+    <button type="submit">Cadastrar</button>
+</form>
   )
 }
